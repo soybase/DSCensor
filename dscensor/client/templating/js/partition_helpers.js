dd18f759f00bc92beb1e6ca1690f824df9f6096a
Connor Cameron <ctc@ncgr.org>
2017-11-29 13:21:44 -0700

    added scatter plot to templating.  Templating now generated from neo4j db

--- dscensor/client/templating/js/partition_helpers.js
+++ dscensor/client/templating/js/partition_helpers.js
@@ -8,7 +8,7 @@ function ancestors(node) {
   return path;
 }
 
-function generateFilters(c, f){
+function histogramFilters(c, f){
         var genes_group = c.group().reduceSum(function(d){
             if (Object.keys(f).length > 0){
                 if (f[d.label]){
@@ -72,12 +72,57 @@ function generateFilters(c, f){
                 return d.lgs;
             }
         });
+        var cds_group = c.group().reduceSum(function(d){
+            if (Object.keys(f).length > 0){
+                if (f[d.label]){
+                    return d.cds;
+                }
+            } else {
+                return d.cds;
+            }
+        });
         var filters = {'genes' : genes_group,
                            'exons' : exons_group,
                            'mrnas' : mrnas_group,
                            'pps'   : pps_group,
                            'ppds'  : ppds_group,
                            'chrs'  : chrs_group,
-                           'lgs'   : lgs_group};
+                           'lgs'   : lgs_group,
+                           'cds'   : cds_group};
         return filters;
 }
+
+function scatterFilters(c, f, x, y){
+    var xmax = 0
+    var ymax = 0
+    var scatdomain = c.dimension(
+        function(d){
+           return [d[x], d[y], d.label, d.org_genus, d.org_species];
+        });
+    var scatgroup = scatdomain.group(function(d){
+        if (Object.keys(f).length > 0){
+            if (f[d[2]]){
+                if (d[0] > xmax){
+                    xmax = d[0]
+                }
+                if (d[1] > ymax){
+                    ymax = d[1]
+                }
+                return [d[0], d[1], d[2], d[3], d[4]];
+            }
+        } else {
+            if (d[0] > xmax){
+                xmax = d[0]
+            }
+            if (d[1] > ymax){
+                ymax = d[1]
+            }
+            return [d[0], d[1], d[2], d[3], d[4]];
+        }
+    });
+    xmax = xmax + Math.floor(xmax/5);
+    ymax = ymax + Math.floor(ymax/10);
+    scatfilters = {'dimension' : scatdomain, 'group' : scatgroup, 'xmax' : xmax, 'ymax' : ymax};
+    return scatfilters;
+}
+
