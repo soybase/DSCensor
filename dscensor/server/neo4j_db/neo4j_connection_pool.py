fe3b86e90c6067d7ed98e52912cd414104a6abdf
Connor Cameron <ctc@ncgr.org>
2018-02-05 14:37:39 -0700

    implemented connection pool #10.  Need to write tests

---
+++ dscensor/server/neo4j_db/neo4j_connection_pool.py
@@ -0,0 +1,28 @@
+import os, sys
+import logging
+import simplejson as json
+from contextlib import contextmanager
+from neo4j.v1 import GraphDatabase, basic_auth
+from dscensor import app, session, g
+
+logger = app.logger
+driver = False
+
+#    '''connects a driver (which serves as a connection pool)
+#       https://neo4j.com/docs/api/python-driver/current/driver.html'''
+bolt = 'bolt:{}:{}'.format(app.config['HOST'], app.config['PORT'])
+driver = GraphDatabase.driver(bolt,
+                              auth=basic_auth(app.config['AUTH'], 
+                                              app.config['PSWD']))
+logger.info('connection succeeded, driver:{}'.format(driver))
+
+@contextmanager
+def get_session():
+    '''Allows the API to get sessions from the driver'''
+    session = False
+    try:
+        session = driver.session()
+        yield session
+    finally:
+        if session:
+            session.close()
